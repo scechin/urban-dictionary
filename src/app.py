1273c5b32c6024050a965670c5588fa0c5318ee2
Alejandro Sanchez <aalejo@gmail.com>
2020-05-01 10:20:16 +0000

    validate dot env

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ import requests, os
 
 # Retrieve your API credentials from the .env file
 if os.getenv("API_KEY") is None or os.getenv("API_KEY") == "":
-    print("Make sure you have created your .env file with your API credentials (look for the .evn.example as an example and replace it with your own API credentials that you got from RapidAPI)")
+    print("ERROR! Make sure you have created your .env file with your API credentials (look for the .evn.example as an example and replace it with your own API credentials that you got from RapidAPI)")
     exit(1)
 
 # Get credentials from the .env file
@@ -10,17 +10,3 @@ API_HOST = os.getenv("API_HOST")
 API_KEY = os.getenv("API_KEY")
 
 # continue with your application here
-
-
-term = input("What are you looking for??")
-
-import requests
-headers = {
-    'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
-    'x-rapidapi-key': API_KEY
-}
-
-response = requests.get(API_HOST+"/define?term="+term, headers=headers)
-body = response.json()
-
-print(body["list"][0]["definition"])
